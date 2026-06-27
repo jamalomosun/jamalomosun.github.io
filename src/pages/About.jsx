6e08dfe5ab56fd2931dffbbbb5ac57cb32baf100
jamalomosun <omosunj@carleton.edu>
2026-06-27 02:10:01 -0500

    added theming

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,1 +1,12 @@
-export default function About() { return <h1>About Me</h1> }
+import { FaSpaceShuttle, FaTerminal, FaCode } from 'react-icons/fa';
+import { BiMath } from 'react-icons/bi';
+import { IoSparkles } from 'react-icons/io5';
+
+export default function About() {
+  return (
+    <div>
+      <h1><IoSparkles /> About Me</h1>
+      <p><FaTerminal /> I love solving complex problems at the intersection of math <BiMath /> and computer science <FaCode />.</p>
+    </div>
+  );
+}
