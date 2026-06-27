53c5b9fe3796fe3ed04f20d33c81c8aa832981fe
jamalomosun <omosunj@carleton.edu>
2026-06-27 01:17:39 -0500

    added information to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,25 @@ function Home() {
     <div className="home-content">
       <h3>Hi, I'm Jamal. I'm currently:</h3>
       <ul>
+        <li>Quantative Researcher @ <a href="https://www.rmichaelalvarez.com/">Alvarez Lab</a></li>
+        <li>Learning about applied mathematics, machine learning, and data science</li>
+        <li>Exploring the intersection of technology, society, and policy</li>
+
      </ul>
       
       <h3>formerly:</h3>
       <ul>
-        </ul>
+        <li>Researching crypto disinformation and AI bias @ <a href="https://www.rmichaelalvarez.com/">Alvarez Lab</a></li>
+        <li>Math + CS @ <a href="https://www.carleton.edu/">Carleton College</a></li>
+        <li>Calculating solar panel soiling @ <a href="https://radiangen.com/">Radian Generation</a></li>
+        <li>Building payment infrastructure @ <a href="https://alphascenario.com/">AlphaScenario</a></li>
+        <li>Sustainability analytics and college policy @ <a href="https://www.carleton.edu/focus/">Carleton FOCUS</a></li>
+        <li>Building sustainable branding with nonprofits @ SerenCo</li>
+
+
+
+
+      </ul>
     </div>
   );
 }
