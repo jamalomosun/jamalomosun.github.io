589bca23709390295055b2ffdbbdab1a7b66442a
jamalomosun <omosunj@carleton.edu>
2026-06-26 22:46:12 -0500

    added placeholder exports to pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,19 @@
-export default function Home() { return <h1>Home</h1> }
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home-content">
+      <h3>Hi, I'm Jamal. I'm currently:</h3>
+      <ul>
+     </ul>
+      
+      <h3>formerly:</h3>
+      <ul>
+        </ul>
+    </div>
+  );
+}
+
+export default Home;
+
+
