aa5dbeadf5fbb2d6a8932aae5730179386dcbc69
jamalomosun <locatejamalo@gmail.com>
2026-08-22 18:24:51 -0500

    Polish miscellaneous links and document workflow

--- src/pages/Misc.jsx
+++ src/pages/Misc.jsx
@@ -5,9 +5,9 @@ export default function Misc() {
 			<p className="proj-meta">Collection of things.</p>
 			<h2>Cool things my friends do:</h2>
 			<ul>
-				<li>Avery<a href="https://substack.com/@aysdunn">Substack</a></li>
-				<li>Revathi<a href="https://revathi770292.substack.com/">Substack</a></li>
-				<li>Lauren<a href="https://www.tiktok.com/@lettersfromjin">TikTok</a></li>
+				<li>Avery: <a href="https://substack.com/@aysdunn">Substack</a></li>
+				<li>Revathi: <a href="https://revathi770292.substack.com/">Substack</a></li>
+				<li>Lauren: <a href="https://www.tiktok.com/@lettersfromjin">TikTok</a></li>
 			</ul>
 		</main>
 	);
